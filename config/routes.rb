aa7914f8c645c82153c658fd98b45ac26bf5e054
teresitasachsen <teresitasachsen99@gmail.com>
2021-03-24 16:40:17 -0600

    created controllers and actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :history, only: [:index]
+  resources :impressum, only: [:index]
+  resources :news, only: [:index, :show]
+  resources :welcome, only: [:index]
 end
