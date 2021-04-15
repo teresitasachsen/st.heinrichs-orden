4de07d3547e69e4b1dde4295c61456d230de14f0
teresitasachsen <teresitasachsen99@gmail.com>
2021-04-15 18:10:25 +0200

    implemented magnifying glass but not fully functional yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,10 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
+import { magnify } from '../plugins/magnifier';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  magnify("hn1", 2);
 });
+
